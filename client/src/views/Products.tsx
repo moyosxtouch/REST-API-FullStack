e1477652eabb94b7d4b174dcad15050554b2960d
moyo <moyosxtouch@gmail.com>
2024-07-31 18:46:17 -0600

    modificacion para mostrar productos

---
+++ client/src/views/Products.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react-refresh/only-export-components */
+import { Link, useLoaderData } from "react-router-dom";
+import { getProducts } from "../services/ProductService";
+import ProductDetails from "../components/ProductDetails";
+import { Product } from "../types";
+
+export async function loader() {
+  const products = await getProducts();
+
+  return products;
+}
+export default function Products() {
+  const products = useLoaderData() as Product[];
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+        <Link
+          to="productos/nuevo"
+          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-slate-500"
+        >
+          Agregar Productos
+        </Link>
+      </div>
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+            <tr>
+              <th className="p-2">Producto</th>
+              <th className="p-2">Precio</th>
+              <th className="p-2">Disponibilidad</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <ProductDetails key={product.id} product={product} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
